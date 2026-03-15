16fb8cb8878b05975ec6ccc0c0edd8f80985c4d0
Sameer8810 <www.sameer0001@gmail.com>
2026-03-15 16:50:25 +0530

    Fix gsap duplicate import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
-import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
+
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+
+export let smoother: ScrollSmoother;
 
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 export let smoother: ScrollSmoother;
